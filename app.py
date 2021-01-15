a0692f748226e93f7cd6f701161cff2f2d9cca50
Dilara Parry <parry.dilara@gmail.com>
2021-01-15 12:23:46 +0100

    streamlit model7

--- app.py
+++ app.py
@@ -9,7 +9,7 @@ Online Mole Detector
 uploaded_file = st.file_uploader("PLEASE UPLOAD A MOLE PICTURE ...", type="jpg")
 if uploaded_file is not None:
     image = Image.open(uploaded_file)
-    st.image(image, caption='Uploaded MRI.', use_column_width=True)
+    st.image(image, caption='File uploaded.', use_column_width=True)
     st.write("")
     st.write("Classifying...")
     label = teachable_machine_classification(image, 'second_model.h5')
