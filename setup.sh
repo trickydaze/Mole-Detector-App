a4d5e86f27685bc2a5ba7d6b5e54103d2d674e4a
Dilara Parry <parry.dilara@gmail.com>
2021-01-16 14:29:12 +0100

    streamlit model7

--- setup.sh
+++ setup.sh
@@ -1,7 +1,12 @@
+
 mkdir -p ~/.streamlit/
-echo “
-[server]
-headless = true
-enableCORS=false
-port = $PORT
-“ > ~/.streamlit/config.toml
+echo "\
+[general]\n\
+email = \"yildizdilara-parry@becode.xyz\"\n\
+" > ~/.streamlit/credentials.toml
+echo "\
+[server]\n\
+headless = true\n\
+enableCORS=false\n\
+port = $PORT\n\
+" > ~/.streamlit/config.toml
